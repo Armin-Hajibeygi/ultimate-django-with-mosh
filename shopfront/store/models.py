0cffd83ff1dda7c7d95f3a9191f81ac0f11c2e1c
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-02 09:02:34 +0330

    Adding membership to Customer model

--- shopfront/store/models.py
+++ shopfront/store/models.py
@@ -9,8 +9,19 @@ class Product(models.Model):
     
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
-    birth_date = models.DateTimeField(null=True)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
