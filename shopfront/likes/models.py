9c9ebbe67de0236246bb508cd6cbdb926db34f5a
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-02 10:59:54 +0330

    Creating likes app

---
+++ shopfront/likes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+class LikedItem(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
