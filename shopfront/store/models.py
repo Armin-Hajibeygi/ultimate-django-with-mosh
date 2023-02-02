3b3dbaf4475d7d9bd02dc7507af3155e2a32dda8
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-02 09:07:06 +0330

    Create Order class

--- shopfront/store/models.py
+++ shopfront/store/models.py
@@ -25,3 +25,18 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'C'),
+        (PAYMENT_STATUS_FAILED, 'F'),
+    ]
+    
+    placed_at = models.DateTimeField(auto_now_add=True) 
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
