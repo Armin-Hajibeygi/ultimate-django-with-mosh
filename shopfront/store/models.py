aa378df7d2a8ba3a37ea0f7cd2053a9252d639c7
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-02 10:33:33 +0330

    Adding Promotion class

--- shopfront/store/models.py
+++ shopfront/store/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
@@ -12,6 +18,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion, related_name='products')
     
 
 class Customer(models.Model):
