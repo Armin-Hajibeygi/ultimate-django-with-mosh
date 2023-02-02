92d8fa289f02a0e1aa05e9b50869be0bdef434aa
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-03 00:40:43 +0330

    Add MySQL

--- shopfront/storefront/settings.py
+++ shopfront/storefront/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'armiN0023500794',
     }
 }
 
