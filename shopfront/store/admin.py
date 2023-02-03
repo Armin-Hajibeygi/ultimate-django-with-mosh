52830a868136248530a6232925867a27857b6c1f
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 02:01:22 +0330

    Create url for orders on customer admin

--- shopfront/store/admin.py
+++ shopfront/store/admin.py
@@ -23,9 +23,27 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
     list_per_page = 10
+    
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            })
+        )
+        
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+    
+        
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
 
 
 @admin.register(models.Order)
