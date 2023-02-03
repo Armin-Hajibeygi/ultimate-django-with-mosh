152bd463a39008340ce8330deca438f89174fab6
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-03 22:27:36 +0330

    Create inventory_status

--- shopfront/store/admin.py
+++ shopfront/store/admin.py
@@ -3,10 +3,15 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_stats']
     list_editable = ['unit_price']
     list_per_page = 10
-
+    
+    @admin.display(ordering='inventory')
+    def inventory_stats(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK' 
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
