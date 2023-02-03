7bfdeac684cd0bbc53fd0bebc9102a10a8552a05
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-03 22:32:50 +0330

    Add collection_title to Product admin

--- shopfront/store/admin.py
+++ shopfront/store/admin.py
@@ -3,15 +3,20 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_stats']
+    list_display = ['title', 'unit_price', 'inventory_stats', 'collection_titles']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+    
+    def collection_titles(self, product):
+        return product.collection.title
     
     @admin.display(ordering='inventory')
     def inventory_stats(self, product):
         if product.inventory < 10:
             return 'Low'
         return 'OK' 
+    
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
