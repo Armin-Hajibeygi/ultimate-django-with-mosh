c9651e524b4fdb9f8350835a43e1e8d19fda2531
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 02:16:59 +0330

    Adding clear_inventory action to Product admin

--- shopfront/store/admin.py
+++ shopfront/store/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
@@ -20,6 +20,7 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_stats', 'collection_titles']
     list_editable = ['unit_price']
     list_per_page = 10
@@ -34,6 +35,15 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return 'Low'
         return 'OK' 
+    
+    @admin.action(description='Clear Inventory')
+    def clear_inventory(self, request, queryset):
+       updated_count = queryset.update(inventory=0)
+       self.message_user(
+           request,
+           f'{updated_count} products were successfully updated',
+           messages.ERROR
+       )
     
 
 @admin.register(models.Customer)
