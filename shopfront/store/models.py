bc29a3d7cff566fed609522554022dcd6f7f47fa
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 02:31:32 +0330

    Minor changes on Product fields

--- shopfront/store/models.py
+++ shopfront/store/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -21,11 +22,14 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(
+        max_digits=6, 
+        decimal_places=2,
+        validators=[MinValueValidator(1)])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion, related_name='products')
+    promotions = models.ManyToManyField(Promotion, related_name='products', blank=True)
     
     def __str__(self):
         return self.title
