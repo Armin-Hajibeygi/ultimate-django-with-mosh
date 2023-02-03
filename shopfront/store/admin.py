9fe0607e28827edd09fd961132ba17904bc253c6
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 02:03:18 +0330

    Adding search field to Customer admin

--- shopfront/store/admin.py
+++ shopfront/store/admin.py
@@ -26,6 +26,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
     list_per_page = 10
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     
     @admin.display(ordering='orders_count')
     def orders(self, customer):
