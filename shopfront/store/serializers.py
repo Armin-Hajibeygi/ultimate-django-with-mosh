51f117b166a15f59006bf8cd47c215c5a90211ce
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 14:02:57 +0330

    Rewrite ProductSerializer with ModelSerializer

--- shopfront/store/serializers.py
+++ shopfront/store/serializers.py
@@ -8,15 +8,12 @@ class CollectionSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(source='unit_price', max_digits=6, decimal_places=2)
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+    
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail',
-    )
     
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
