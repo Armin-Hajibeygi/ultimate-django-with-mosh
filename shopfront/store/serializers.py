ce26a8e64ad05335749de6cf6224430675c097ff
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 13:51:50 +0330

    Create CollectionSerializer

--- shopfront/store/serializers.py
+++ shopfront/store/serializers.py
@@ -3,12 +3,17 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(source='unit_price', max_digits=6, decimal_places=2)
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
     
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
