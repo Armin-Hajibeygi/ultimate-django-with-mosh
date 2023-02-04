54e019c667c2173a491d43dea822449fee7d0d96
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 17:31:40 +0330

    Add DELETE method to product_detail view

--- shopfront/store/views.py
+++ shopfront/store/views.py
@@ -20,17 +20,25 @@ def product_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
+    
     if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+    
     elif request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response({'error': 'Product cannot be deleted'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
 
 
