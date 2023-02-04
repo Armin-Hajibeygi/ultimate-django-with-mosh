b5c3e2ad6010d4660e3e702219a124e82bf1e073
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 13:57:40 +0330

    Make CollectionSerializer hyperlink

--- shopfront/store/serializers.py
+++ shopfront/store/serializers.py
@@ -13,7 +13,10 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(source='unit_price', max_digits=6, decimal_places=2)
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name='collection-detail',
+    )
     
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
