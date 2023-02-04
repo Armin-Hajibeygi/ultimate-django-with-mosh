176d7425908713d25f86bdec6734974d0e1c3ce0
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-02-04 12:40:47 +0330

    Create restful api for product_detail

--- shopfront/store/views.py
+++ shopfront/store/views.py
@@ -7,3 +7,8 @@ from rest_framework.response import Response
 @api_view()
 def product_list(request):
     return Response('ok')
+
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
